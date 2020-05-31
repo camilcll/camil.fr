982d6f92a663b3ab7dfaef9cff93cbcb3621d3b2
COULLON CAMIL p1804059 <camil.coullon@etu.univ-lyon1.fr>
2020-05-31 23:37:27 +0200

    ajout bootstrap + creation class main.js

--- script/main.js
+++ script/main.js
@@ -1,45 +1,45 @@
 
 /*** CHANGING TEXT ***/
-var TxtRotate = function(el, toRotate, period) {
-  this.toRotate = toRotate;
-  this.el = el;
-  this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
-  this.txt = '';
-  this.tick();
-  this.isDeleting = false;
-};
-
-TxtRotate.prototype.tick = function() {
-  var i = this.loopNum % this.toRotate.length;
-  var fullTxt = this.toRotate[i];
-
-  if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-  } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-  }
-
-  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-  var that = this;
-  var delta = 250 - Math.random() * 100;
-
-  if (this.isDeleting) { delta /= 2; }
-
-  if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === '') {
+class TxtRotate {
+  constructor(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
     this.isDeleting = false;
-    this.loopNum++;
-    delta = 400;
   }
+  tick() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    }
+    else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+    var that = this;
+    var delta = 250 - Math.random() * 100;
+    if (this.isDeleting) {
+      delta /= 2;
+    }
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    }
+    else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 400;
+    }
+    setTimeout(function () {
+      that.tick();
+    }, delta);
+  }
+}
 
-  setTimeout(function() {
-    that.tick();
-  }, delta);
-};
 
 window.onload = function() {
   var elements = document.getElementsByClassName('txt-rotate');
@@ -59,7 +59,3 @@ window.onload = function() {
 /* END OF CHANGING TEXT */
 
 
-
-/* UNDER MAINTAINANCE */
-window.alert("Ce site est en cours de développement");
-/* END OF UNDER MAINTAINANCE */
